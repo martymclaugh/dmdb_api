3bc630c556d1ce92d11a7f3a05c2757b330c776c
Marty McLaughlin <martymclaugh@gmail.com>
2017-02-19 13:14:34 -0800

    add method to create movie, or increase movie views by one

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,14 @@
 class MoviesController < ApplicationController
   def create
-    p "here"
-    p params
+    @movie = Movie.find_by(imdb_id: params[:imdb_id])
+    if @movie
+      @movie.update(views: @movie.views + 1)
+    else
+      @movie = Movie.create(imdb_id: params[:imdb_id], poster_path: params[:poster_path])
+    end
   end
 
   def index
-    p "hello world"
+    # grab most recently created movies
   end
 end
