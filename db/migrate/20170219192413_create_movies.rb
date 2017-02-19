42dc60fd4378eb8471f8643ac299e9f17524605d
Marty McLaughlin <martymclaugh@gmail.com>
2017-02-19 11:29:22 -0800

    add fields to movies table

--- db/migrate/20170219192413_create_movies.rb
+++ db/migrate/20170219192413_create_movies.rb
@@ -1,7 +1,8 @@
 class CreateMovies < ActiveRecord::Migration[5.0]
   def change
     create_table :movies do |t|
-
+      t.string :imdb_id
+      t.string :poster_path
       t.timestamps
     end
   end
